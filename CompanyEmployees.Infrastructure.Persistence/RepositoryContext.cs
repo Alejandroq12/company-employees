95bee27ea4d7b44fb4ef3a15860119821030e0ad
Julio Quezada <jquezada@dom.gob.sv>
2025-05-23 17:29:07 -0600

    feat: start implemented context factory

---
+++ CompanyEmployees.Infrastructure.Persistence/RepositoryContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+namespace CompanyEmployees.Infrastructure.Persistence;
+
+public class RepositoryContext : DbContext
+{
+  public RepositoryContext(DbContextOptions options) : base(options)
+  {
+  }
+
+  public DbSet<Company>? Companies { get; set; }
+  public DbSet<Employee>? Employees { get; set; }
+}
